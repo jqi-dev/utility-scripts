22d48b65a5ece45936783580375864ca3e55140e
Sean Kelley <sean@sean-macbook.local>
2017-03-28 15:48:09 -0400

    simple single author search functionality

--- arxivbot/arxivbot.py
+++ arxivbot/arxivbot.py
@@ -34,22 +34,27 @@ with open('jqi-fellows.csv','rb') as f:
         jqi_fellows.append(row[0])
 
 
-def reform_name(fellow):
-    name = fellow.split()
-    author = name[-1] + '_' + '_'.join(name[0:-1])
-    return author
+def reform_name(author):
+    n = author.split()
+    n = n[-1] + '_' + '_'.join(n[0:-1])
+    return n
 
 
-def print_papers(paper_list, fellow, message_string):
+def print_papers(paper_list, author):
+
+    message = []
+
     if len(paper_list) > 0:
-        message_string.append('\n*' + fellow + '*')
+        message.append('\n*' + author + '*')
         for paper in paper_list:
-            message_string.append('\n' + paper)
+            message.append('\n' + paper)
+
+    return message
 
 
-def get_papers(fellow, time, message_string):
+def get_papers(author, days):
 
-    author = reform_name(fellow)
+    author = reform_name(author)
 
     url = 'http://export.arxiv.org/api/query?search_query=au:+' + author + '&sortBy=lastUpdatedDate&sortOrder=descending'
     data = urllib.urlopen(url).read()
@@ -62,11 +67,12 @@ def get_papers(fellow, time, message_string):
         datetime_object = datetime.datetime.strptime(date, '%Y-%m-%d').date()
         today = datetime.date.today()
         diff = abs(today - datetime_object).days
-        if diff <= time:
+        if diff <= days:
             title = entry.title.cdata.replace("\n ", "")
             link = entry.id.cdata
             paper_list.append(title + ' ' + link)
-    print_papers(paper_list, fellow, message_string)
+
+    return print_papers(paper_list, author)
 
 
 def experimental_search(affiliation_flag, pages):
@@ -111,26 +117,36 @@ def experimental_search(affiliation_flag, pages):
 
     return "Could not find any papers with affiliation *" + affiliation_flag + "*\n"
 
-# arxivbot ID as an environment variable
-BOT_ID = os.environ.get("BOT_ID")
+def return_search(author_list, days):
+    message = []
+    for author in author_list:
+        message += get_papers(author, days)
+    response = "Papers updated in arXiv from the last " + str(days) + " days:" + ''.join(message)
+    return response
 
-# constants
-AT_BOT = "<@" + BOT_ID + ">"
+# # arxivbot ID as an environment variable
+# BOT_ID = os.environ.get("BOT_ID")
+#
+# # constants
+# AT_BOT = "<@" + BOT_ID + ">"
+#
+# # instantiate Slack client
+# slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
-# instantiate Slack client
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
-
-def handle_command(command, channel):
+# def handle_command(command, channel):
+def handle_command(command):
     """
         Receives commands directed at the bot and determines if they
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    response = "Not sure what you mean. Use *author* followed by " + \
-               "jqi/quics/phys/cnam/cmtc and a number of days. Or, use " + \
-               "*affiliation* followed by jqi/phys/quics/cnam/cmtc and a number of pages. " + \
-               "(The number of pages defaults to 1.)"
+    response = "Not sure what you mean." + \
+               "\n_For fellows search_: Use *author* followed by jqi/quics/phys/cnam/cmtc and a number of days. " + \
+               "(The number of days defaults to 30.)" + \
+               "\n_For affiliation search_: Use *affiliation* followed by jqi/phys/quics/cnam/cmtc and a number of pages. " + \
+               "(The number of pages defaults to 1.)" + \
+               "\n_For individual search_: Use *search* followed by the name. Returns papers from past 30 days)."
 
     # Possible commands start with "author" or "affiliation"
     if command.startswith("author"):
@@ -143,17 +159,13 @@ def handle_command(command, channel):
             author_search_list = author_lookup.get(author_flag, jqi_fellows)
 
             try:
-                days = int(command.split()[2]) # days should probably default to some value, like 30 or something
-                if days <= 180:
-                    message = []
-                    for author in author_search_list:
-                        get_papers(author, days, message)
-                        response = "Papers updated in arXiv from the last " + str(days) + " days:" + ''.join(message)
-                else:
-                    response = "Please limit days to fewer than 180."
+                days = int(command.split()[2]) # check to see if value for days is given
             except:
-                print "Couldn't get the days"
-                pass
+                print "Number of days for fellows search not specified; defaulting to 30"
+                days = 30
+
+            response = return_search(author_search_list, days)
+
         except:
             print "Couldn't get the flag"
             pass
@@ -170,34 +182,43 @@ def handle_command(command, channel):
 
         response = experimental_search(affiliation_flag, pages)
 
-    slack_client.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
+    if command.startswith("search"):
+        print "Fufilling individual search!"
+
+        # use everything after 'search' as the search term
+        name = command.split(' ', 1)[1]
+        response = return_search([name], 30)
+
+    print response
+
+    # slack_client.api_call("chat.postMessage", channel=channel,
+    #                       text=response, as_user=True)
 
 
-def parse_slack_output(slack_rtm_output):
-    """
-        The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
-    output_list = slack_rtm_output
-    if output_list and len(output_list) > 0:
-        for output in output_list:
-            if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
-    return None, None
-
-
-if __name__ == "__main__":
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
-    if slack_client.rtm_connect():
-        print("arXivbot connected and running!")
-        while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
-            time.sleep(READ_WEBSOCKET_DELAY)
-    else:
-        print("Connection failed. Invalid Slack token or bot ID?")
+# def parse_slack_output(slack_rtm_output):
+#     """
+#         The Slack Real Time Messaging API is an events firehose.
+#         this parsing function returns None unless a message is
+#         directed at the Bot, based on its ID.
+#     """
+#     output_list = slack_rtm_output
+#     if output_list and len(output_list) > 0:
+#         for output in output_list:
+#             if output and 'text' in output and AT_BOT in output['text']:
+#                 # return text after the @ mention, whitespace removed
+#                 return output['text'].split(AT_BOT)[1].strip().lower(), \
+#                        output['channel']
+#     return None, None
+#
+#
+# if __name__ == "__main__":
+#     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+#     if slack_client.rtm_connect():
+#         print("arXivbot connected and running!")
+#         while True:
+#             command, channel = parse_slack_output(slack_client.rtm_read())
+#             if command and channel:
+#                 handle_command(command, channel)
+#             time.sleep(READ_WEBSOCKET_DELAY)
+#     else:
+#         print("Connection failed. Invalid Slack token or bot ID?")
