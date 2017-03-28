a2ef00f4285e66bbc28f0eaab7b25cd5d42c522c
Sean Kelley <sean@sean-macbook.local>
2017-03-28 15:51:12 -0400

    restoring commented sections

--- arxivbot/arxivbot.py
+++ arxivbot/arxivbot.py
@@ -124,18 +124,17 @@ def return_search(author_list, days):
     response = "Papers updated in arXiv from the last " + str(days) + " days:" + ''.join(message)
     return response
 
-# # arxivbot ID as an environment variable
-# BOT_ID = os.environ.get("BOT_ID")
-#
-# # constants
-# AT_BOT = "<@" + BOT_ID + ">"
-#
-# # instantiate Slack client
-# slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+# arxivbot ID as an environment variable
+BOT_ID = os.environ.get("BOT_ID")
+
+# constants
+AT_BOT = "<@" + BOT_ID + ">"
+
+# instantiate Slack client
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 
-# def handle_command(command, channel):
-def handle_command(command):
+def handle_command(command, channel):
     """
         Receives commands directed at the bot and determines if they
         are valid commands. If so, then acts on the commands. If not,
@@ -191,34 +190,34 @@ def handle_command(command):
 
     print response
 
-    # slack_client.api_call("chat.postMessage", channel=channel,
-    #                       text=response, as_user=True)
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
 
 
-# def parse_slack_output(slack_rtm_output):
-#     """
-#         The Slack Real Time Messaging API is an events firehose.
-#         this parsing function returns None unless a message is
-#         directed at the Bot, based on its ID.
-#     """
-#     output_list = slack_rtm_output
-#     if output_list and len(output_list) > 0:
-#         for output in output_list:
-#             if output and 'text' in output and AT_BOT in output['text']:
-#                 # return text after the @ mention, whitespace removed
-#                 return output['text'].split(AT_BOT)[1].strip().lower(), \
-#                        output['channel']
-#     return None, None
-#
-#
-# if __name__ == "__main__":
-#     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
-#     if slack_client.rtm_connect():
-#         print("arXivbot connected and running!")
-#         while True:
-#             command, channel = parse_slack_output(slack_client.rtm_read())
-#             if command and channel:
-#                 handle_command(command, channel)
-#             time.sleep(READ_WEBSOCKET_DELAY)
-#     else:
-#         print("Connection failed. Invalid Slack token or bot ID?")
+def parse_slack_output(slack_rtm_output):
+    """
+        The Slack Real Time Messaging API is an events firehose.
+        this parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                       output['channel']
+    return None, None
+
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("arXivbot connected and running!")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
